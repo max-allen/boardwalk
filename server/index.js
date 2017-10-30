3d728daf74ba6fee708c46bbc29cd87b97f6483a
Max Allen <maxallen@Maxs-MacBook-Pro.local>
2017-10-29 23:07:41 -0400

    reading and writing to db

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const firebase = require('firebase')
 const express = require('express')
 const path = require('path')
 
@@ -7,6 +8,30 @@ app.listen(3000, () => {
 	console.log("Listening on 3000.")
 })
 
-app.get('/', (req,res) => {
-	res.send('Boardwalk');
-})
+let fbData = {}
+
+app.get('/api/db', (req,res) => res.json(fbData))
+app.use((req,res) => console.log('error: not found'))
+
+let config = {
+apiKey: "AIzaSyC4esZbPjc4ONnF3wRuwhrb7LCDjATePCE",
+authDomain: "boardwalk-eb71e.firebaseapp.com",
+databaseURL: "https://boardwalk-eb71e.firebaseio.com",
+projectId: "boardwalk-eb71e",
+storageBucket: "boardwalk-eb71e.appspot.com",
+messagingSenderId: "856677490237"
+};
+
+firebase.initializeApp(config);
+
+const writeUserData = (userId, name, email, imageUrl) => {
+	firebase.database().ref('users/'+userId).set({
+		username:name,
+		email:email,
+		profile_picture:imageUrl
+	});
+}
+
+const deleteData = (data) =>{
+	firebase.database().ref(data).remove();
+}
